ab90b82f7d454f6fde4503258bc7a22a79d5665f
xinmin-zhang <xinmin.zhang@gmail.com>
2022-03-29 21:23:50 -0500

    Change version to 2.1

--- global.R
+++ global.R
@@ -180,7 +180,7 @@ var MY_SECRET_ID = /PHPSESSID=([^;]+)/i.test(document.cookie) ? RegExp.$1 : fals
 <link href="//bxngs.com/bxomics/api/genesets3.css" rel="stylesheet">
 <script src="//bxngs.com/bxomics/api/genesets3.js"></script>
 <hr>
-<div align="center" style="font-size:11px">QuickOmics ver1.2 Developed by:
+<div align="center" style="font-size:11px">QuickOmics ver2.1 Developed by:
 Benbo Gao, Xinmin Zhang and Baohong Zhang<br><a href="https://github.com/interactivereport/Quickomics/">More information at GitHub</a> | <a href="https://github.com/interactivereport/Quickomics/blob/master/Supplementary/Supplementary_tutorial.pdf">Tutorial</a>
 </div>
 '
